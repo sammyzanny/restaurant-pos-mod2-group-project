bcc82886d6e489f432933185288593a4abd0830e
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-11 15:03:53 -0600

    Work on modifications edit form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :servers
-  resources :foods
+  resources :foods do
+    resources :modifications only: [:new, :create]
+  end
   resources :orders
   resources :checks
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
