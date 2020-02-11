6981c9c77d73f60adb6782d7e2fb155cf6b83813
m-gizzi <matthewgizzi@verizon.net>
2020-02-11 14:43:20 -0600

    Debugged total method

--- app/models/check.rb
+++ app/models/check.rb
@@ -20,6 +20,12 @@ class Check < ApplicationRecord
   def total
     food_prices = self.foods.map{|food| food.price}
     mod_prices = self.modifications.map{|mod| mod.price}
+    if food_prices.reduce(:+) == nil
+      food_prices = [0]
+    end
+    if mod_prices.reduce(:+) == nil
+      mod_prices = [0]
+    end
     food_prices.reduce(:+) + mod_prices.reduce(:+)
   end
   
