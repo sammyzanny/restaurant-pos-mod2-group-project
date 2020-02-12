6b7102b3a3c6f00869d663614495053d258419d5
Zach Weber <weberzt@gmail.com>
2020-02-12 15:16:25 -0600

    updated modifications controller and modification new form

--- app/controllers/modifications_controller.rb
+++ app/controllers/modifications_controller.rb
@@ -3,6 +3,7 @@ class ModificationsController < ApplicationController
   def new
     @modification = Modification.new
     @orders = Order.all
+    @mod_prices = [0.00, 0.50, 1.00, 1.50, 2.00, 2.50, 3.00, 4.00, 5.00]
   end
 
   def edit
